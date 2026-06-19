cd34f349fa46f84046d1f4d50e8105169dd8f688
maimouna-mt2206944 <163984098+maimouna-mt2206944@users.noreply.github.com>
2026-06-19 13:40:26 +0300

    created the base website

---
+++ bayanqa/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        
+        
+      </Head>
+     <p>
+THIS IS THE STARTING LOCATIONS
+     </p>
+     
+    </>
+  );
+}
