d026e0263345cddbfa39852456d184bc0aab7d2c
adibamaisha <adibamaisha2042@gmail.com>
2026-06-20 17:47:08 +0300

    ai agent added prop

--- bayanqa/pages/_app.js
+++ bayanqa/pages/_app.js
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import AiAgentWidget from "@/components/AiAgentWidget";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <AiAgentWidget />
       <Footer />
     </>
   );
